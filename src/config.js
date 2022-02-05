23e9d165084c6b54c2fe66c367b2036bf00417d0
tjferrara <tjferrara@gmail.com>
2022-02-05 11:51:48 -0800

    Update API key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: Replace next line with your own public API key
-  builderAPIKey: '59bb518773c14842921abe05d5e2bee3'
+  builderAPIKey: '8db27bf272b0494591ff3ce5ce971643'
 };
